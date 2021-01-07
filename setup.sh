9008ffd501ee866e9a28e3546ab49694320a1c82
Nick Beattie <nickbytes@Nicks-MacBook-Air.local>
2021-01-07 15:18:47 -0800

    Add config

--- setup.sh
+++ setup.sh
@@ -5,3 +5,14 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 rm -rf ~/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+# gitignore
+GITIGNORE_FILE="~/.gitignore"
+rm -f "$GITIGNORE_FILE"
+ln -s "$~/dotfiles/.gitignore_global" "$GITIGNORE_FILE"
+
+# gitconfig
+GITCONFIG_FILE="~/.gitconfig"
+rm -f "$GITCONFIG_FILE"
+ln -s ~/dotfiles/.gitconfig "$GITCONFIG_FILE"
+
