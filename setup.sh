487dd4d71b573e983eeae22a3afd88539c35d7a1
Nick Beattie <nick@nickbytes.com>
2021-10-27 22:39:22 -0700

    Adds apps and homebrew

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env zsh
 
+if ! type "brew" > /dev/null; then
+    echo ""
+    echo "Installing Homebrew..." 
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+else
+    echo ""
+    echo "Homebrew is already installed. Skipping."
+fi
+
 # Update system
 brew update
 brew upgrade
